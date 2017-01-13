a029cf974415b9da9d745113227bc8a253153ecf
Jonathan Andersson <jonathan.andersson@simonsoft.se>
2017-01-13 13:01:47 +0100

    Errors extends ResponseProxy
    errorMessage no longer exists, using entity instead which maps to json
    attr `body`

--- runtime/src/main/java/org/lambadaframework/runtime/models/error/BadRequestResponse.java
+++ runtime/src/main/java/org/lambadaframework/runtime/models/error/BadRequestResponse.java
@@ -3,15 +3,14 @@ package org.lambadaframework.runtime.models.error;
 
 public class BadRequestResponse extends ErrorResponse {
 
-    protected String errorMessage;
 
     public BadRequestResponse() {
-        this.errorMessage = "Bad request";
+        this.entity = "Bad request";
         this.code = 400;
     }
 
     public BadRequestResponse(String errorMessage) {
         this();
-        this.errorMessage = errorMessage;
+        this.entity = errorMessage;
     }
 }
